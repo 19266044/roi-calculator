0341065548bec1517b70e68fcf7d8960d8fb6945
19266044 <192666044@qq.com>
2025-06-30 11:26:44 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,17 +8,7 @@
           <a href="/" class="flex text-base text-white font-bold">
             <span class="text-orange">Keywords &nbsp;</span>ROI Calculator
           </a>
-          <div class="hidden flex-row items-center justify-center space-x-4 sm:flex sm:space-x-10">
-            <div class="flex flex-row items-center justify-center space-x-2 sm:space-x-4">
-              <a
-                href="https://github.com/Kunsect/roi-calculator"
-                type="button"
-                class="flex w-full min-w-max flex-row items-center justify-center space-x-2 rounded-lg bg-medium-blue py-2 px-3 text-sm font-semibold text-white shadow-button-secondary transition-all duration-100 hover:bg-medium-blueHover active:bg-medium-blue sm:w-fit sm:px-5 sm:text-base"
-              >
-                <span>Github</span>
-              </a>
-            </div>
-          </div>
+
           <div
             class="absolute bottom-0 left-0 h-[1px] w-screen bg-medium-blue sm:-translate-x-10"
           ></div>
@@ -33,12 +23,7 @@
     >
       <div class="flex w-full max-w-7xl flex-col items-center justify-center space-y-16">
         <div class="flex flex-col items-center justify-center space-y-4">
-          <a
-            href="https://github.com/Kunsect/roi-calculator"
-            rel="noopener noreferrer"
-            target="_blank"
-            class="transition-all duration-200 hover:-rotate-12 hover:scale-110 active:scale-90 active:duration-100"
-          >
+
             <svg
               aria-hidden="true"
               class="h-10 w-10 fill-gray-500 group-hover:fill-gray-300"
